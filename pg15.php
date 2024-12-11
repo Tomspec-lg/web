55960706b848d53482c7fdb10a97b3a46f4f6b43
Tomspec-lg <josephtom046@gmail.com>
2024-12-10 21:13:42 -0800

    Delete pg15.php

--- pg15.php
+++
@@ -1,9 +0,0 @@
-<?php
-$a=array("Anjana","christina","blessy","angela");
-print_r($a);
-echo("<br>Ascending order:");
-asort($a);
-print_r($a);echo("<br> Descending order:");
-Asort($a);
-print_r($a);
-?>
