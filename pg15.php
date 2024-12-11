c7685cab977a60dc9be20fedaab5b0d8186b5fd5
Tomspec-lg <josephtom046@gmail.com>
2024-12-10 21:06:41 -0800

    Add files via upload

---
+++ pg15.php
@@ -0,0 +1,9 @@
+<?php
+$a=array("Anjana","christina","blessy","angela");
+print_r($a);
+echo("<br>Ascending order:");
+asort($a);
+print_r($a);echo("<br> Descending order:");
+Asort($a);
+print_r($a);
+?>
---
+++ pg16.php
@@ -0,0 +1,19 @@
+<?php
+$cricket=array(array('m s dhoni',40),
+array('virat kohli',34),
+array('sachin tendulkar',48),
+array('rohith sharma',31),
+array('sanju samson',27));
+?>
+<html>
+<table border="1"><tr><th>Name</th><th>Age</th></tr>
+<tr><td><?php echo $cricket[0][0]?></td><td><?php echo $cricket[0][1]?><?/td></tr>
+<tr><td><?php echo $cricket[1][0]?></td><td><?php echo $cricket[1][1]?><?/td></tr>
+<tr><td><?php echo $cricket[2][0]?></td><td><?php echo $cricket[2][1]?><?/td></tr>
+<tr><td><?php echo $cricket[3][0]?></td><td><?php echo $cricket[3][1]?><?/td></tr>
+<tr><td><?php echo $cricket[4][0]?></td><td><?php echo $cricket[4][1]?><?/td></tr>
+</table>
+</html>
+
+
+
